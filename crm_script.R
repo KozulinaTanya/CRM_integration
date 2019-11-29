7552932af7bb78efdd69977a661d9995f66ff51e
KozulinaTanya <46816240+KozulinaTanya@users.noreply.github.com>
2019-11-29 11:21:47 +0300

    Update crm_script.R

--- crm_script.R
+++ crm_script.R
@@ -17,11 +17,24 @@ holder <- tbl(con, "NAME_OF_TABLE2") %>%
   filter(EMAIL=="EMAIL@EMAIL.COM")%>% 
   collect()
 
+#ltv_by_email
+ltv <- left_join(EMAILS,
+                   tbl(con, "NAME_OF_TABLE") %>% 
+                     select(ORDERDATETIME, AMOUNTNET, EMAIL, CITY)%>%
+                     filter(ORDERDATETIME >= "2016-09-22" & ORDERDATETIME <= "2019-11-25") %>%
+                     group_by(EMAIL, CITY) %>%
+                     summarise(SUM = sum(AMOUNTNET)) %>%
+                     collect(), by = "EMAIL")
 
-deliv <- left_join(orders,
-                   tbl(con, "NAME_OF_TABLE3") %>%
-                     filter(ORDERID %in% order_numbers)%>%
-                     select(CSKUNAME255, MERSUBTMNAME, ORDERID, SIZENAME, INVENTSIZEID)%>%
-                     collect(), by = "ORDERID")
+#function grade byltv
+grade_by_ltv <- function(x){
+  for (i in x){
+if (i<= 57000) {print("small")} else if (i >= 57000 & i <=23000) {print("central")} else {print("big")}
+  }
+}
+
+#connect emails with grade
+ltv %>% 
+  mutate(new = grade(SUM))
 
 
